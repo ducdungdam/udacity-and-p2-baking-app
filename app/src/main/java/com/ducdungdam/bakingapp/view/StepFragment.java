5b5240f9ead5b3dcd0ffd8b64c349d1c13bce314
Duc Dung Dam <ducdungdam@gmail.com>
2018-06-22 23:31:35 +0200

    Adds TextUtils.isEmpty to check for a null or empty string

--- app/src/main/java/com/ducdungdam/bakingapp/view/StepFragment.java
+++ app/src/main/java/com/ducdungdam/bakingapp/view/StepFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -110,8 +111,7 @@ public class StepFragment extends Fragment implements Player.EventListener {
   }
 
   private void initializePlayer() {
-    if (exoPlayer == null && step != null && step.getVideoUrl() != null && !step.getVideoUrl()
-        .isEmpty()) {
+    if (exoPlayer == null &&  step != null && TextUtils.isEmpty(step.getVideoUrl())) {
       // Create an instance of the ExoPlayer.
       TrackSelector trackSelector = new DefaultTrackSelector();
       LoadControl loadControl = new DefaultLoadControl();
