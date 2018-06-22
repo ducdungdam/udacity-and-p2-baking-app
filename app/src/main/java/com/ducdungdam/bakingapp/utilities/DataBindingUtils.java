f85373df88c6a66ee9c74f9b540d0b96725a0e11
Duc Dung Dam <ducdungdam@gmail.com>
2018-06-23 00:18:16 +0200

    Handles image url for recipes

--- app/src/main/java/com/ducdungdam/bakingapp/utilities/DataBindingUtils.java
+++ app/src/main/java/com/ducdungdam/bakingapp/utilities/DataBindingUtils.java
@@ -7,7 +7,26 @@ import com.squareup.picasso.Picasso;
 
 public class DataBindingUtils {
   /**
-   * Loads an Step Thumbnail by a given Url to an ImageView.
+   * Loads an recipe image by a given url to an ImageView.
+   *
+   * @param view ImageView in which the Image is load into
+   * @param imageUrl Url of recipe image
+   */
+  @BindingAdapter("setRecipeImage")
+  public static void setRecipeImage(ImageView view, String imageUrl) {
+    try {
+      Picasso.with(view.getContext())
+          .load(imageUrl)
+          .error(R.drawable.img_recipe_card)
+          .placeholder(R.drawable.img_recipe_card)
+          .into(view);
+    } catch (IllegalArgumentException e) {
+      Picasso.with(view.getContext()).load(R.drawable.img_recipe_card);
+    }
+  }
+
+  /**
+   * Loads an step thumbnail by a given url to an ImageView.
    *
    * @param view ImageView in which the Image is load into
    * @param thumbnailUrl Url of step thumbnail
